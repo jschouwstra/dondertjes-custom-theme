7d346106aa86746970adf4eb6354ddc740db8047
jschouwstra <schouwstra.jelle@gmail.com>
2015-08-03 00:32:46 +0200

    Start with new theme

---
+++ portfolio.php
@@ -0,0 +1,14 @@
+
+<br/>
+<div class="panel panel-default col-md-6 col-centered">
+	<div class="panel-body">
+<div>
+    <img src="<?php echo get_template_directory_uri(); ?>/images/html-icon.png" height="200" class=" wow rollIn " data-wow-duration="1s" >
+    <img src="<?php echo get_template_directory_uri(); ?>/images/css-icon.png" class=" wow rollIn " data-wow-duration="2s">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/responsive-icon.png" class=" wow rollIn " data-wow-duration="3s">
+
+</div></br>
+</div>
+</div>
+
+ 
